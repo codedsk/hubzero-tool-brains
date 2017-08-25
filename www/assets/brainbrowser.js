9b8738e583807cf46f1a0e0d24edb2c32e529100
codedsk <telldsk@gmail.com>
2017-08-25 08:41:54 -0400

    unzipping models, updating js to show dbs model

--- www/assets/brainbrowser.js
+++ www/assets/brainbrowser.js
@@ -1,6 +1,8 @@
 $(function() {
     "use strict";
 
+    var THREE = BrainBrowser.SurfaceViewer.THREE;
+
     // setup brainbrowser widget.
     window.viewer = BrainBrowser.SurfaceViewer.start("brainbrowser", function(viewer) {
 
@@ -12,8 +14,59 @@ $(function() {
         // Start rendering the scene.
         viewer.render();
 
-        // Load a model into the scene.
-        viewer.loadModelFromURL("/assets/models/brain-surface1.obj");
+
+        // Load the dbs model into the scene.
+        // This will load 3 surfaces into the viewer:
+        //   dbs.json
+        //   dbs-fibers.json
+        //   dbs-vat.json
+
+
+        viewer.annotations.setMarkerRadius(0.3);
+        viewer.loadModelFromURL("/assets/models/dbs.json", {
+            format: "json",
+            complete: function() {
+                var i;
+
+                for (i = 17; i <= 93; i++) {
+                    viewer.setTransparency(0.8, {
+                        shape_name: "dbs.json_" + 1
+                    });
+                }
+            }
+        });
+
+        viewer.loadModelFromURL("/assets/models/dbs-fibers.json", {
+          format: "json",
+          complete: function() {
+            var i;
+
+            for (i = 1; i <= 664; i++) {
+              viewer.setTransparency(0.6, {
+                shape_name: "dbs-fibers.json_" + i
+              });
+            }
+          }
+        });
+
+        viewer.loadModelFromURL("/assets/models/dbs-vat.json", {
+          format: "json"
+        });
+
+        viewer.zoom = 1.8;
+
+        var matrixRotX, matrixRotY, matrixRotZ;
+        matrixRotX = new THREE.Matrix4();
+        matrixRotX.makeRotationX(-0.5 * Math.PI);
+        matrixRotY = new THREE.Matrix4();
+        matrixRotY.makeRotationY(-0.8 * Math.PI);
+        matrixRotZ = new THREE.Matrix4();
+        matrixRotZ.makeRotationZ(-0.1 * Math.PI);
+
+        viewer.model.applyMatrix(matrixRotY.multiply(matrixRotZ.multiply(matrixRotX)));
+
+        /// end dbs model
+
 
         // Load a colormap
         viewer.loadColorMapFromURL("/assets/color-maps/spectral1.txt");
